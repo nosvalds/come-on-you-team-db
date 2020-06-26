7c9b7b960fb022ba6996956b9c8f5767c50ef860
Nik Osvalds <nosvalds@gmail.com>
2020-06-26 17:16:55 +0100

    Revert "try to fix CORS error"
    This reverts commit 75091ab441c8d6cbdeb590c7049a8d6dbe25856b.

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
